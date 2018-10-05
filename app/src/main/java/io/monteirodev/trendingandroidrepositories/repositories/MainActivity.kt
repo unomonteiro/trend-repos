54d4f25068d16b909dd27d873c0c27ad3a868e18
Carlos Monteiro <offbyte@gmail.com>
2018-10-05 18:22:18 +0100

    add tablet layout

--- app/src/main/java/io/monteirodev/trendingandroidrepositories/repositories/MainActivity.kt
+++ app/src/main/java/io/monteirodev/trendingandroidrepositories/repositories/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView.NO_POSITION
 import android.widget.LinearLayout.VERTICAL
 import android.widget.Toast
 import io.monteirodev.trendingandroidrepositories.R
 import io.monteirodev.trendingandroidrepositories.details.DetailsActivity
+import io.monteirodev.trendingandroidrepositories.details.DetailsFragment
 import io.monteirodev.trendingandroidrepositories.models.Repository
 import io.monteirodev.trendingandroidrepositories.network.ReposManager
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -19,9 +21,15 @@ class MainActivity : AppCompatActivity() {
 
     protected var subscriptions = CompositeDisposable()
 
-    private val REPO_LIST_KEY = "REPO_LIST_KEY"
+    private val REPO_LIST_KEY = "repo_list_key"
     private val repoAdapter by lazy { RepositoryAdapter(this::repoClicked) }
     private val reposManager by lazy { ReposManager() }
+
+    private val isTablet by lazy { resources.getBoolean(R.bool.is_tablet) }
+    private val REPO_INDEX_KEY = "repo_index"
+    private var repoIndex = NO_POSITION
+    private var detailsFragment = DetailsFragment()
+    private lateinit var repository: Repository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,9 +56,13 @@ class MainActivity : AppCompatActivity() {
         subscriptions.clear()
     }
 
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putParcelableArrayList(REPO_LIST_KEY, repoAdapter.getRepos())
+        if (isTablet) {
+            outState.putInt(REPO_INDEX_KEY, repoIndex)
+        }
     }
 
     private fun initAdapter() {
@@ -69,6 +81,12 @@ class MainActivity : AppCompatActivity() {
                 .subscribe(
                         { retrievedRepos ->
                             repoAdapter.addRepos(retrievedRepos)
+                            if (isTablet) {
+                                if (repoIndex == NO_POSITION) {
+                                    repoIndex = 0
+                                }
+                                replaceRepoDetailsFragment(retrievedRepos[repoIndex])
+                            }
                         },
                         { e ->
                             Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
@@ -78,7 +96,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun repoClicked(repository: Repository) {
-        val intent = DetailsActivity.newIntent(this, repository)
-        startActivity(intent)
+        if (isTablet) {
+            repoIndex = repoAdapter.getRepos().indexOf(repository)
+            setRepoDetailsFragment(repository)
+        } else {
+            val intent = DetailsActivity.newIntent(this, repository)
+            startActivity(intent)
+        }
+    }
+
+
+    private fun setRepoDetailsFragment(repository: Repository) {
+        detailsFragment = DetailsFragment.newInstance(repository)
+        replaceRepoDetailsFragment(repository)
+    }
+
+
+    private fun replaceRepoDetailsFragment(newRepository: Repository) {
+        repository = newRepository
+        detailsFragment = DetailsFragment.newInstance(repository)
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.details_container, detailsFragment)
+                .commit()
     }
 }
